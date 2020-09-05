db0efc14bb2e7b67a522254b3cc9c3e7f516622e
mic <micmor.m@gmail.com>
2020-09-05 13:21:29 -0400

    implement custom hook in useapplicationDada.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,124 +4,114 @@ import "components/Application.scss";
 import Appointment from "components/Appointment/index";
 import getAppointmentsForDay from "../helpers/selectors";
 import { getInterview, getInterviewersForDay } from "../helpers/selectors";
+import useApplicationData from "../hooks/useApplicationData";
 const axios = require('axios').default;
 
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
-  const setDay = day => setState({ ...state, day });
-  //const setDays = days => setState(prev => ({ ...prev, days }));
+  //this was before use useApplicationData ***********************************
+  // const [state, setState] = useState({
+  //   day: "Monday",
+  //   days: [],
+  //   appointments: {},
+  //   interviewers: {}
+  // });
 
-  //original use state as reference
-  //const [day, setDay] = useState("Monday");
-  //const [days, setDays] = useState([]);
-
+  // 1 moved to useApplicationData ***********************************
+  //const setDay = day => setState({ ...state, day });
+  
+// 2 moved to useApplicationData ***********************************
 //get data from the server
-  useEffect(() => {
-    const promise1 =axios.get("http://localhost:8001/api/days")
-    const promise2 =axios.get("http://localhost:8001/api/appointments")
-    const promise3 =axios.get("http://localhost:8001/api/interviewers")
+  // useEffect(() => {
+  //   const promise1 =axios.get("http://localhost:8001/api/days")
+  //   const promise2 =axios.get("http://localhost:8001/api/appointments")
+  //   const promise3 =axios.get("http://localhost:8001/api/interviewers")
     
-    Promise.all([
-      Promise.resolve(promise1),
-      Promise.resolve(promise2),
-      Promise.resolve(promise3)
-    ]).then((all) => {
-      setState(prev => ({ days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-    })    
-      //.then(response => setDays(response.data));
-    }, []);
+  //   Promise.all([
+  //     Promise.resolve(promise1),
+  //     Promise.resolve(promise2),
+  //     Promise.resolve(promise3)
+  //   ]).then((all) => {
+  //     setState(prev => ({ days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+  //   })    
+  //     //.then(response => setDays(response.data));
+  //   }, []);
 
   //filter from the state only the interviewer of the day
   const interviewers = getInterviewersForDay(state, state.day)
-    console.log("Interviewers of the day:", interviewers)
+    //console.log("Interviewers of the day:", interviewers)
 
   //filter from the state only the appointment of the day
   const appointments = getAppointmentsForDay(state, state.day)
   
-  //take the value from the lower component form and update the state in app level
+
+  // 3 moved to useApplicationData ***********************************
   //to save a new interview booking in the database
-  function bookInterview(id, interview) {
+  // function bookInterview(id, interview) {
     
-    //create new appointment 
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    //to update in appoiments the appointment with same id
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
+  //   //create new appointment 
+  //   const appointment = {
+  //     ...state.appointments[id],
+  //     interview: { ...interview }
+  //   };
+  //   //to update in appoiments the appointment with same id
+  //   const appointments = {
+  //     ...state.appointments,
+  //     [id]: appointment
+  //   };
  
-    //update the server state
-    const res = axios.put(`http://localhost:8001/api/appointments/${id}`, {interview: {...interview}})
-    
-    //I have to return the promise to use
-    //it inside the save function in the form component
-    return Promise.resolve(res)
-    .then((res) => {
-      console.log("res data:", res)
-      //set the local state with new value only after the data in the server have been saved
-      //because the server only hold the true data
-      setState({
-        ...state,
-        appointments
-      });
-      //console.log("State", state)
-    })
-  }
+  //   //update the server state
+  //   const res = axios.put(`http://localhost:8001/api/appointments/${id}`, {interview: {...interview}})
 
-  function cancelInterview(id) {
-    // const cancelInterview = (id) => () => {
-    //   return axios.delete(`/api/appointments/${id}`)
-    //update the server state
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-      };
-    //to update in appoiments the appointment with same id
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
+  //   //it inside the save function in the form component
+  //   return Promise.resolve(res)
+  //   .then((res) => {
+  //     console.log("res data:", res)
+  //     //set the local state with new value only after the data in the server have been saved
+  //     //because the server only hold the true data
+  //     setState({
+  //       ...state,
+  //       appointments
+  //     });
+      
+  //   })
+  // }
+
+
+  // 4 moved to useApplicationData ***********************************
+  // function cancelInterview(id) {
+  //     //update the server state
+  //   const appointment = {
+  //     ...state.appointments[id],
+  //     interview: null
+  //     };
+  //   //to update in appoiments the appointment with same id
+  //   const appointments = {
+  //     ...state.appointments,
+  //     [id]: appointment
+  //   };
    
-    const res =  axios.delete(`http://localhost:8001/api/appointments/${id}`) //, {interview: null})
-    //I have to return the promise to use
-    //it inside the onDelete function in the form component
-    return Promise.resolve(res)
-      .then((res) => {
-     // console.log("res data from Cancel interview:", res)
-      setState({
-        ...state,
-        appointments
-      });
+  //   const res =  axios.delete(`http://localhost:8001/api/appointments/${id}`)
+  //   //I have to return the promise to use
+  //   //it inside the onDelete function in the form component
+  //   return Promise.resolve(res)
+  //     .then((res) => {
+  //    // console.log("res data from Cancel interview:", res)
+  //     setState({
+  //       ...state,
+  //       appointments
+  //     });
 
-    //   //set the local state with new value only after the data in the server have been saved
-    //   //because the server only hold the true data
-    //   //let copyState = {...state.appointments[id].interview}
-    //   //console.log("copyState", copyState)
-    //   //copyState = null
-    
-    //   //Is this the proper way to set the state for appointment to NULL?
-    //   // setState(prev => ({
-    //   //   ...state.appointments[id],
-    //   //   interview: null
-    //   // }))
-    })
-  }
-
-
-
-
-
+  //   })
+  // }
 
 
   return (
