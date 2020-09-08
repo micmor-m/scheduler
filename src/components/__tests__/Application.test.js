07f45fbeed054e636e92aa5b4c116091d3d3f3e3
mic <micmor.m@gmail.com>
2020-09-08 16:46:38 -0400

    complete implementation of the test in application, books an interview and reduces the spots remaining for Monday by 1

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, act, prettyDOM, getAllByTestId, getByText, getByAltText, getByPlaceholderText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, act, prettyDOM, getAllByTestId, getByText, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 import Form from "components/Appointment/Form";
@@ -56,14 +56,14 @@ describe("Application", () => {
     //console.log(prettyDOM(container));
   });
 
-  it("loads data, books an interview and reduces the spots remaining for Monday by 1 copy3", async () => {
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
     const { container } = render(<Application />);
   
     await waitForElement(() => getByText(container, "Archie Cohen"));
     //appointment is an empty appointment
     const appointments = getAllByTestId(container, "appointment");
     const appointment = appointments[0];
-    console.log(prettyDOM(appointment));
+    //console.log(prettyDOM(appointment));
     //console.log(prettyDOM(container));
     //1)simulate the user click onadd
     fireEvent.click(getByAltText(appointment, "Add"));
@@ -77,15 +77,23 @@ describe("Application", () => {
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     //5)simulate to save the form  
     fireEvent.click(getByText(appointment, "Save"));
-    console.log("This is from line 80", prettyDOM(appointment));
+    //console.log("This is from line 80", prettyDOM(appointment));
     const { container1, debug } = render(<Application />);
     //console.log("This is from debug", prettyDOM(debug()));
     //console.log("*************DEBUG***************")
     //debug()
     //expect(getByText(appointment, "Saving")).toBeInTheDocument();
     await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
-    console.log("*************DEBUG***************")
-    debug()
+    //console.log("*************DEBUG***************")
+    //debug()
+    //Find the specific day node that contains the text "Monday
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+    
+    //console.log("Day", prettyDOM(day));
+    //expect(day(/student name cannot be blank/i)).toBeInTheDocument();
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 
 
